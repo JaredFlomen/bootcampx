50414ea023b760b07b0b77debb31a911f3e5e543
Jared <jaredflomen@gmail.com>
2020-11-23 19:45:05 +0000

    Query with JOIN

--- 2_queries_joins/cohort_total_assignment_duration.sql
+++ 2_queries_joins/cohort_total_assignment_duration.sql
@@ -1,5 +1,6 @@
 SELECT sum(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
+-- This is what I did before, keeping to self check
 -- JOIN cohorts ON cohorts.id = assignment_submissions.assignment_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
